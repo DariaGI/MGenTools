98d6045fa94afee63fa82fc03640bb9247c7cafb
DariaGI <daria_gutnik@mail.ru>
2023-04-06 02:23:04 +0800

    Permanova+Anosim updated

--- ComparativeGenomics-master/FlaskApp/hadlers/vsualize_statistics.py
+++ ComparativeGenomics-master/FlaskApp/hadlers/vsualize_statistics.py
@@ -22,13 +22,12 @@ def match(table_to, table_from):
 
 
 def buildScatter(data, components, strains, predictions):
-    components.insert(0, 'Strain', strains.to_list())
-    components.loc[:, 'Breakdown Type'] = 'unknown'
-    components.loc[:, 'Cluster'] = 0
+    components["Strain"] = data["Strain"]
+    components['Breakdown Type'] = 'unknown'
     if not data.getBreakdown().is_empty():
         match(components, data.getBreakdown())
     if len(predictions) > 0:
-        components.loc[:, 'Cluster'] = predictions
+        components['Cluster'] = predictions
 
     components["Cluster"] = 'Cluster # ' + components["Cluster"].astype(str)
     fig = px.scatter(components, x='Component 1', y='Component 2', color='Breakdown Type', symbol='Cluster',
@@ -41,10 +40,16 @@ def buildScatter(data, components, strains, predictions):
     return pltJSON
 
 
-def clusterization(genes_count, features, clusterMethods, n_clusters, linkage):
+def clusterization(genes_count, features, clusterMethods, n_clusters, linkage, distance, tree, otu_ids):
     predictions = []
-    x = genes_count[:, features].to_numpy()
-    x = StandardScaler().fit_transform(x)
+
+    if distance == "eucledian":
+        x = genes_count[:, features].to_numpy()
+        x = StandardScaler().fit_transform(x)
+
+    else:
+        x = precomputed_matrix(genes_count, distance, tree, otu_ids)[0].to_numpy()
+        x = StandardScaler().fit_transform(x)
 
     if '0' in clusterMethods:
         model = KMeans(n_clusters=int(n_clusters), n_init='auto')
@@ -62,7 +67,7 @@ def clusterization(genes_count, features, clusterMethods, n_clusters, linkage):
     return x, predictions
 
 
-def buildPlots(data, methods, perplexity, clusterMethods, n_clusters, linkage):
+def buildPlots(data, methods, perplexity, metric, clusterMethods, n_clusters, linkage, distance, tree, otu_ids):
     plots = {}
     genes_count = data.getCount()
     genes_count = genes_count[[s.name for s in genes_count if not (s.null_count() == genes_count.height)]]
@@ -75,22 +80,50 @@ def buildPlots(data, methods, perplexity, clusterMethods, n_clusters, linkage):
     if len(strains) > 1:
         if len(features) > 1:
 
-            x, predictions = clusterization(genes_count, features, clusterMethods, n_clusters, linkage)
+            if distance == "eucledian":
+                x, predictions = clusterization(genes_count, features, clusterMethods,
+                                                n_clusters, linkage, distance, tree, otu_ids)
+                if '0' in methods:
+                    # only eucledian distance
+                    methodData = PCA(n_components=2, random_state=0)
+                    components = pd.DataFrame(data=methodData.fit_transform(x), columns=['Component 1', 'Component 2'])
+                    plots['PCA'] = buildScatter(data, components, strains, predictions)
 
-            if '0' in methods:
-                methodData = PCA(n_components=2, random_state=0)
-                components = pd.DataFrame(data=methodData.fit_transform(x), columns=['Component 1', 'Component 2'])
-                plots['PCA'] = buildScatter(data, components, strains, predictions)
+                if '1' in methods:
+                    methodData = MDS(random_state=0, metric=metric, dissimilarity=distance)
+                    components = pd.DataFrame(data=methodData.fit_transform(x), columns=['Component 1', 'Component 2'])
+                    plots['MDS'] = buildScatter(data, components, strains, predictions)
 
-            if '1' in methods:
-                methodData = MDS(random_state=0)
-                components = pd.DataFrame(data=methodData.fit_transform(x), columns=['Component 1', 'Component 2'])
-                plots['MDS'] = buildScatter(data, components, strains, predictions)
+                if '2' in methods:
+                    methodData = TSNE(random_state=0, perplexity=int(perplexity), metric=distance)
+                    components = pd.DataFrame(data=methodData.fit_transform(x), columns=['Component 1', 'Component 2'])
+                    plots['t-SNE'] = buildScatter(data, components, strains, predictions)
 
-            if '2' in methods:
-                methodData = TSNE(random_state=0, perplexity=int(perplexity))
-                components = pd.DataFrame(data=methodData.fit_transform(x), columns=['Component 1', 'Component 2'])
-                plots['t-SNE'] = buildScatter(data, components, strains, predictions)
+            else:
+                type_of_matrix = "precomputed"
+
+                if '0' in methods:
+                    # only eucledian distance
+                    x, predictions = clusterization(genes_count, features, clusterMethods,
+                                                    n_clusters, linkage, distance="eucledian")
+                    methodData = PCA(n_components=2, random_state=0)
+                    components = pd.DataFrame(data=methodData.fit_transform(x), columns=['Component 1', 'Component 2'])
+                    plots['PCA'] = buildScatter(data, components, strains, predictions)
+
+                else:
+                    x, predictions = clusterization(genes_count, features, clusterMethods,
+                                                    n_clusters, linkage, distance)
+                    if '1' in methods:
+                        methodData = MDS(random_state=0, metric=metric, dissimilarity=type_of_matrix)
+                        components = pd.DataFrame(data=methodData.fit_transform(x),
+                                                  columns=['Component 1', 'Component 2'])
+                        plots['MDS'] = buildScatter(data, components, strains, predictions)
+
+                    if '2' in methods:
+                        methodData = TSNE(random_state=0, perplexity=int(perplexity), metric=type_of_matrix)
+                        components = pd.DataFrame(data=methodData.fit_transform(x),
+                                                  columns=['Component 1', 'Component 2'])
+                        plots['t-SNE'] = buildScatter(data, components, strains, predictions)
 
         else:
             x = []
@@ -106,10 +139,10 @@ def buildPlots(data, methods, perplexity, clusterMethods, n_clusters, linkage):
     return plots
 
 
-def statistic_test(data, distance, tree, otu_ids, statMethods, matrix, perplexity, clusterMethods, n_clusters, linkage):
+def statistic_test(data, distance, tree, otu_ids, statMethods, perplexity, clusterMethods, n_clusters, linkage):
     genes_count = data.getCount()
     test_result = {}
-    bc_dm, strains, features = matrix(genes_count, distance, tree, otu_ids)
+    bc_dm, strains, features = precomputed_matrix(genes_count, distance, tree, otu_ids)
     if len(bc_dm) != 0:
         x, predictions = clusterization(genes_count, features, perplexity, clusterMethods, n_clusters, linkage)
         sample_md = pd.DataFrame(predictions, index=list(strains), columns=["subject"])
@@ -121,23 +154,26 @@ def statistic_test(data, distance, tree, otu_ids, statMethods, matrix, perplexit
     return test_result
 
 
-def matrix(genes_count, distance, tree, otu_ids):
+def precomputed_matrix(genes_count, distance, tree, otu_ids):
     bc_dm = []
     if genes_count.is_empty():
         return bc_dm
 
     strains = genes_count["Strain"]
     features = genes_count.columns[1:]
-    genes_count_cut = np.array(genes_count[features])
+    genes_count_cut = np.array(genes_count[:, features])
 
     if len(strains) > 1:
         if len(features) > 1:
-            if "0" in distance:
-                bc_dm = beta_diversity("euclidean", genes_count_cut, strains)
-            if "1" in distance:
-                bc_dm = beta_diversity("braycurtis", genes_count_cut, strains)
-            if "2" in distance:
-                bc_dm = beta_diversity("weighted_unifrac", genes_count_cut, strains, tree=tree, otu_ids=otu_ids)
-            if "3" in distance:
-                bc_dm = beta_diversity("unweighted_unifrac", genes_count_cut, strains, tree=tree, otu_ids=otu_ids)
+            bc_dm = beta_diversity(distance, genes_count_cut, strains, tree=tree, otu_ids=otu_ids)
+            # if distance == "euclidean":
+            #     bc_dm = beta_diversity(distance, genes_count_cut, strains)
+            # if distance == "braycurtis":
+            #     bc_dm = beta_diversity(distance, genes_count_cut, strains)
+            # if distance == "weighted_unifrac":
+            #     bc_dm = beta_diversity(distance, genes_count_cut, strains, tree=tree, otu_ids=otu_ids)
+            # if distance == "unweighted_unifrac":
+            #     bc_dm = beta_diversity(distance, genes_count_cut, strains, tree=tree, otu_ids=otu_ids)
+            # if distance == "jaccard":
+            #     bc_dm = beta_diversity(distance, genes_count_cut, strains)
     return bc_dm, strains, features
