81cd2d514387e2b9077181ca15a98e94fbf1d6a2
DariaGI <daria_gutnik@mail.ru>
2023-04-11 00:38:35 +0800

    Changed to list for categories and dictionary for systems counting function

--- ComparativeGenomics-master/FlaskApp/hadlers/counter.py
+++ ComparativeGenomics-master/FlaskApp/hadlers/counter.py
@@ -1,30 +1,31 @@
 import pandas as pd
 import polars as pl
 import pyarrow
+import numpy as np
 
 
 def countFunctions(data, categories, systems):
-    ranks = ['Category', 'System']
     files = data.getClassified()
-    count = pd.DataFrame([], columns=list(set(categories + systems)))
+    categories_names = ["C_" + i for i in categories]
+    systems_names = ["S_" + key + "_" + sys for key in systems for sys in systems[key]]
+    strains = [filename for filename in files]
+    count = pd.DataFrame(0, index=np.arange(len(strains)), columns=["Strain"] + categories_names + systems_names)
+    count["Strain"] = strains
 
-    count.insert(0, 'Strain', [filename for filename in files])
-    count.iloc[:, 1:] = 0
+    for strain_name in files:
+        strain_data = files[strain_name]
+        index = count.loc[count['Strain'] == strain_name].index
 
-
-    for filename in files:
-        file = files[filename]
-        index = count.loc[count['Strain'] == filename].index
-
-        for count_s in count.columns:
-            if count_s != count.columns[0]:
-                if count_s in categories:
-                    param = ranks[0]
-                else:
-                    param = ranks[1]
-
-                count_s = count_s.replace("?", " ")
-                count.loc[(index, count_s)] = len(file.filter(pl.col(param).str.contains(count_s)))
+        for count_feature in count.columns[1:]:
+            classified_feature = count_feature.replace("?", " ").split("_")
+            if classified_feature[0] == "C":
+                count.loc[(index, count_feature)] = len(
+                    strain_data.filter(pl.col("Category").str.contains(classified_feature[1])))
+            else:
+                count.loc[(index, count_feature)] = len(
+                    strain_data.filter((pl.col("Category").str.contains(classified_feature[1]))
+                                       & (pl.col("System").str.contains(classified_feature[2]))))
 
     count = pl.from_pandas(count)
     return count.sort(['Strain'])
+
