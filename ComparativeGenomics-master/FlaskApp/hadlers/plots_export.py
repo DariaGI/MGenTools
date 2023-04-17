81cb9c7f486189e2f473d0d1a8deb334b915c325
DariaGI <daria_gutnik@mail.ru>
2023-04-17 23:51:13 +0800

    Permanova+Anosim add plots_export function with zip archive of all plots

--- ComparativeGenomics-master/FlaskApp/hadlers/plots_export.py
+++ ComparativeGenomics-master/FlaskApp/hadlers/plots_export.py
@@ -1,9 +1,19 @@
 import plotly
+import zipfile
 
 def export_plots(data, export_format):
     plots_dict = data.getPlots()
+    plots_to_zip = []
     for name_plots in plots_dict:
         exported_plot = plotly.io.read_json(plots_dict[name_plots])
         if export_format in ["png", "jpeg", "svg", "pdf"]:
-            exported_plot.write_image(name_plots + "." + export_format)
+            plot_file = name_plots + "." + export_format
+            exported_plot.write_image(plot_file)
+            plots_to_zip.append(plot_file)
+    zf = zipfile.ZipFile("all_plots.zip", "w")
+    for files in plots_to_zip:
+        zf.write(files)
+    zf.close()
+    return "all_plots.zip"
+
 
